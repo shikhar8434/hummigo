cdcb24fe2b4dcab8642d9ae2c1d87270b7bc19fe
Abhineet Pandey <54185164+abhineetpandey10@users.noreply.github.com>
2021-04-02 22:09:35 +0530

    Added React App

---
+++ hummigoWeb/hummigo/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div class='titlebar'>
+          <img src='./assets/hummigologo.png' alt='Hummigo Logo'/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
